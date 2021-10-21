adc225f6292d40055b34eca2c8a5a146d4d2f3df
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-21 12:56:58 +0200

    dificultando el reto

--- src/main/java/retoDia21/appRetoDia21/controlador/retoNuevoEndpoint.java
+++ src/main/java/retoDia21/appRetoDia21/controlador/retoNuevoEndpoint.java
@@ -9,26 +9,34 @@ import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.stereotype.Component;
 
+import retoDia21.appRetoDia21.model.Persona;
+
 @Component
 @Endpoint(id= "fiesta")
 public class retoNuevoEndpoint {
 	
-	private List<String> personas = new ArrayList<>();
+	private List<Persona> personas = new ArrayList<>();
+	Persona persona1;
+	
 	
 	@ReadOperation
-	public List<String> fiesta(){
+	public List<Persona> fiesta(){
 		return personas;
 	}
 	
 	@WriteOperation
-	public void setAsistentes(@Selector String nuevoAsistente){
-		personas.add(nuevoAsistente);
+	public void setAsistentes(@Selector String nombre){
+		
+		persona1.setNombre(nombre);
+		
+		
+		personas.add(persona1);
 		
 	}
 	
 	@ReadOperation
-	public void deleteOperation(@Selector String borrarAsistente){
-		personas.remove(borrarAsistente);
+	public void deleteAsistente(@Selector Persona persona){
+		personas.remove(persona);
 	}
 	
 	
